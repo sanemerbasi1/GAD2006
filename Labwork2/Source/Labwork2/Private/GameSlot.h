32c23c924c8f094cb15c809239163e1375852db3
sanemerbasi1 <sanemerbasi@gmail.com>
2026-05-19 18:13:44 +0300

    labwork2

---
+++ Labwork2/Source/Labwork2/Private/GameSlot.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "GameSlot.generated.h"
+
+USTRUCT(Blueprintable)
+struct FSGridPosition
+{
+	GENERATED_USTRUCT_BODY();
+
+	FSGridPosition() {}
+	FSGridPosition(int col, int row) : Col(col), Row(row) {}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	uint8 Col;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	uint8 Row;
+};
+
+UENUM(Blueprintable)
+enum EGridState
+{
+	GS_Default,
+	GS_Highlighted,
+	GS_Offensive,
+	GS_Supportive,
+};
+
+UCLASS()
+class AGameSlot : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGameSlot();
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* Box;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Plane;
+
+	FSGridPosition GridPosition;
+
+	EGridState GridState;
+
+	UFUNCTION()
+	void SetState(EGridState NewState);
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
